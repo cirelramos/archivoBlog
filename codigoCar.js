4dddad64cf06902be9c8fd847f97281f0a5d653e
cirelramos <cirelramos@gmail.com>
2017-02-22 22:52:16 -0400

    Update codigoCar.js

--- codigoCar.js
+++ codigoCar.js
@@ -57,7 +57,7 @@ function enviarDatos(params) {
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     http.onreadystatechange = function() { //Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
-            // console.log(http.responseText);
+            console.log(http.responseText);
         }
     }
     http.send(params);
